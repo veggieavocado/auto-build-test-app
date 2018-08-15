9b75f4726b8fcf79c2b12e280b5d066eb550f2ac
1 <ppark9553@gmail.com>
2018-08-15 17:18:24 +0900

    트레비스 빌드 재시도

--- travisResult.js
+++ travisResult.js
@@ -3,16 +3,23 @@ const axios = require('axios');
 const rainStateURL = '';
 
 const sendTestResult = async () => {
-  const testResult = process.env.TRAVIS_TEST_RESULT;
+  const testResult = process.env.TRAVIS_TEST_RESULT || 'NONE';
   let result = '';
   if (testResult == 0) {
     result = 'FAIL';
-  } else {
+  } else if (testResult == 1) {
     result = 'PASS';
+  } else if (testResult == 'NONE') {
+    console.log(`Test Result: ${testResult}`);
   }
-  const resultData = {
-    app: 'test_app',
-    state: result
+
+  if (testResult != 'NONE') {
+    const resultData = {
+      app: 'test_app',
+      state: result
+    }
+    console.log(resultData);
   }
-  console.log(resultData);
 }
+
+sendTestResult();
