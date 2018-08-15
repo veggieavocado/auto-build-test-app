f9c020689476d31b57a4749cb010c1fc6eab4c0e
1 <ppark9553@gmail.com>
2018-08-15 17:12:40 +0900

    데이터 형식 변화

--- travisResult.js
+++ travisResult.js
@@ -4,5 +4,15 @@ const rainStateURL = '';
 
 const sendTestResult = async () => {
   const testResult = process.env.TRAVIS_TEST_RESULT;
-  console.log(testResult);
+  let result = '';
+  if (testResult == 0) {
+    result = 'FAIL';
+  } else {
+    result = 'PASS';
+  }
+  const resultData = {
+    app: 'test_app',
+    state: result
+  }
+  console.log(resultData);
 }
